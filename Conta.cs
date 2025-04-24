3c7402d6ea0f6ac50eedead25635b30dd82564d4
Rafael <rafael.a.darold10@gmail.com>
2025-04-23 20:24:25 -0400

    Inicio do projeto

---
+++ Conta.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml;
+
+public class Conta
+{
+    public int idAgencia;
+    public string numeroConta;
+    public string nomeProprietario;
+    public double saldo;
+    public void Depositar(double valorDep)
+    {
+        if (valorDep <= 0)
+        {
+            Console.WriteLine("Não é possivel realizar depositos menor que zero");
+        }
+        else
+        {
+            saldo += valorDep;
+
+            Console.WriteLine("\nDeposito realizado com sucesso!");
+            Console.WriteLine($"Saldo atual: R${saldo}");
+        }
+    }
+
+    public void Sacar(double valorSaq)
+    {
+        if(saldo <= 0)
+        {
+            Console.WriteLine("Saque não permitido, saldo insuficiente!");
+        }
+        else if(saldo < valorSaq)
+        {
+            Console.WriteLine("Saque não permitido, saldo insuficiente!");
+        }
+        else
+        {
+            saldo -= valorSaq;
+
+            Console.WriteLine("\nSaque realizado com sucesso!");
+            Console.WriteLine($"Saldo Atual: R${saldo}");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿
+
+using static System.Net.Mime.MediaTypeNames;
+
+Console.WriteLine("---Banco Darold---\n");
+
+List<Conta> listaContaBanc = new List<Conta>();
+
+while (true)
+{
+    Console.ReadKey();
+    Console.Clear();
+    Console.Write("1 para cadastrar uma nova CONTA:\n2 para LISTAR as contas:\n3 realizar Operações Bancarias:\n4 para SAIR:\n\nOpção:");
+    int op = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    Console.WriteLine();
+
+    if (op == 1)
+    {
+        Conta conta = new Conta();
+
+        Console.WriteLine("Cadastre sua Conta");
+        Console.Write("Informe o ID da conta: ");
+        conta.idAgencia = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Informe o número da conta: ");
+        conta.numeroConta = Console.ReadLine();
+
+        Console.WriteLine("Informe o nome do proprietario da conta: ");
+        conta.nomeProprietario = Console.ReadLine();
+
+
+        listaContaBanc.Add(conta);
+
+        Console.WriteLine("CONTA CADASTRADA COM SUCESSO!");
+    }
+    else if (op == 2)
+    {
+        foreach (Conta cont in listaContaBanc)
+        {
+            Console.WriteLine($"ID: {cont.idAgencia} - Nº CONTA: {cont.numeroConta} " +
+                $"- NOME: {cont.nomeProprietario} - SALDO: {cont.saldo.ToString("C2")}");
+        }
+    }
+    else if (op == 3)
+    {
+        Console.Write("Informe o numero da sua conta:");
+        Conta conta = listaContaBanc.Find(conta => conta.idAgencia == Convert.ToInt32(Console.ReadLine()));
+        if (conta != null)
+        {
+
+            while (true)
+            {
+                Console.ReadKey();
+                Console.Clear();
+                Console.Write("Digite 1 para DEPOSITAR, 2 para SACAR ou 3 para SAIR: ");
+                op = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine();
+
+                if (op == 1)
+                {
+                    Console.Write("Informe o valor que deseja depositar:");
+                    conta.Depositar(Convert.ToDouble(Console.ReadLine()));
+                }
+                else if (op == 2)
+                {
+                    Console.WriteLine("Realizar saque.");
+                    Console.Write("Informe o valor que deseja sacar");
+                    conta.Sacar(Convert.ToDouble(Console.ReadLine()));
+                }
+                else if (op == 3)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Opção invalida!");
+                }
+            }
+        }
+    }
+    else if (op == 4)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Opção invalida!");
+    }
+    
+}
